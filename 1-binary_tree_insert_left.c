5dbf9cc0141308258bec81ed3288b8dec977c49a
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-04 10:12:29 +0100

    task 1 another attempt

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,15 +11,17 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_node;
+
+
 	if (parent == NULL)
 		return (NULL);
-
-	binary_tree_t *new_node;
 
 	new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
 	{
+		free(new_node);
 		return (NULL);
 	}
 
--- binary_trees.h
+++ binary_trees.h
@@ -6,33 +6,35 @@
 #include <unistd.h>
 
 /**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
+* struct binary_tree_s - Binary tree node
+*
+* @n: Integer stored in the node
+* @parent: Pointer to the parent node
+* @left: Pointer to the left child node
+* @right: Pointer to the right child node
+*/
 struct binary_tree_s
 {
-    int n; /* the value */
-    struct binary_tree_s *parent; /* the parent node */
-    struct binary_tree_s *left;   /* the left child node */
-    struct binary_tree_s *right;  /* the left child node */
+	int n; /* the value */
+
+	struct binary_tree_s *parent; /* the parent node */
+	struct binary_tree_s *left;   /* the left child node */
+	struct binary_tree_s *right;  /* the left child node */
 };
 
-typedef struct binary_tree_s binary_tree_t; /* using typdef to define our sturcture as a type */
-
-typedef struct binary_tree_s bst_t; /* binary seach tree */
-
-typedef struct binary_tree_s avl_t; /* AVL tree */
-
-typedef struct binary_tree_s heap_t; /* max heap tree */
-
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value); /* function prototype */
-
-void binary_tree_print(const binary_tree_t *tree); /* function prototype */
-
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value); /* function prototype */
+/* using typdef to define our sturcture as a type */
+typedef struct binary_tree_s binary_tree_t;
+/* binary seach tree */
+typedef struct binary_tree_s bst_t;
+/* AVL tree */
+typedef struct binary_tree_s avl_t;
+/* max heap tree */
+typedef struct binary_tree_s heap_t;
+/* function prototype */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+/* function prototype */
+void binary_tree_print(const binary_tree_t *tree);
+/* function prototype */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif
