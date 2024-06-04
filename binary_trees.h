b3ae099ef5938046a6b3dcf2981d81a0f9a6adf1
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-04 16:06:26 +0100

    task 12 bugs fiex

--- binary_trees.h
+++ binary_trees.h
@@ -56,5 +56,7 @@ size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 /* function prototype */
 size_t binary_tree_size(const binary_tree_t *tree);
+/* function prototype */
+size_t binary_tree_leaves(const binary_tree_t *tree);
 
 #endif
