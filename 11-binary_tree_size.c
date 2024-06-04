47ee852e72325443b24c53cc51ce89710a2ec467
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-04 15:48:48 +0100

    task 11 another attempt attempt

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,8 +2,17 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	size_t size_binary_tree;
+
 	if (tree == NULL)
 		return (0);
 
-	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right + 1));
+	size_binary_tree = 0;
+
+	size_binary_tree += binary_tree_size(tree->left);
+	size_binary_tree += binary_tree_size(tree->right);
+	size_binary_tree++;
+
+
+	return (size_binary_tree);
 }
