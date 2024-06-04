2e8bf79a08db16fc84a60a230b9a1757e1c81cd8
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-04 11:13:31 +0100

    task 3 betty fixed

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,15 +1,22 @@
 #include "binary_trees.h"
 
+/**
+* binary_tree_delete - deletes a tree
+*
+* @tree: the tree to delete
+*
+* Descrtiption: a function that deletes a tree
+*/
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
+	{
 		return;
-
+	}
 	else
 	{
 		binary_tree_delete(tree->left);
 		binary_tree_delete(tree->right);
 		free(tree);
 	}
-
 }
