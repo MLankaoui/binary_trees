c9ab0f456aaad1704ecf7f00b369f27115ed4a1a
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-04 09:22:43 +0100

    fixed betty coding style

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_node - creates a binary tree node
+ *
+ * @parent: a parameter to specify the parent
+ * @value: to specify the value
+ *
+ * Descrtiption: a function that creates a binary tree node
+ * Return: the newnode if it doesn t fail. NULL otherwise
+*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
