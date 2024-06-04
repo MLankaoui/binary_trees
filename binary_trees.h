9808edb0b85eee09e01cefcd1e4f9a8ded712730
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-04 11:06:22 +0100

    task 3 first attempt

--- binary_trees.h
+++ binary_trees.h
@@ -39,6 +39,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 /* function prototype */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 /* function prototype */
-void binary_tree_delete(binary_tree_t *tree)
+void binary_tree_delete(binary_tree_t *tree);
 
 #endif
