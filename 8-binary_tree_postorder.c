64d2e13ff13b91869f202754898b6dca2a1a3fba
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-04 14:56:45 +0100

    task 8 first attempt

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -13,7 +13,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	binary_tree_inorder(tree->left, func);
-	binary_tree_inorder(tree->right, func);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
 	func(tree->n);
 }
