06e89c05dd627f001b75a58c02a8088c4a82291b
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-04 15:38:40 +0100

    task 10 betty

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,6 +1,13 @@
 #include "binary_trees.h"
 
-
+/**
+ * binary_tree_depth - calculates the dept
+ *
+ * @tree: the tree
+ *
+ * Descrtiption: a function that calculates the dept of a tree
+ * Return: depth
+*/
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (tree == NULL || tree->parent == NULL)
