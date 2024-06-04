57d4f05d1a56b3e8a9b2c5da0fbcacd08b399365
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-04 15:43:11 +0100

    task 11 first attempt

--- binary_trees.h
+++ binary_trees.h
@@ -54,5 +54,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 /* function prototype */
 size_t binary_tree_depth(const binary_tree_t *tree);
+/* function prototype */
+size_t binary_tree_size(const binary_tree_t *tree);
 
 #endif
