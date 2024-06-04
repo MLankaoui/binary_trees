e12b0d06ba98949c544e0eceeb0e53106f2ab7d4
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-04 16:09:08 +0100

    task 12 betty

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_leaves - counts the leaves of a tree
+ *
+ * @tree: the node
+ *
+ * Descrtiption: a function counts the leaves of a tree
+ * Return: number of leaves
+*/
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (tree == NULL)
