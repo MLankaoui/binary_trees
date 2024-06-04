35d6ea0ca2739a8a7d78fd34f9fd7ff22bf09c00
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-04 11:29:59 +0100

    task 5 first attempt

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->parent == NULL)
+		return (1);
+
+	else
+		return (0);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -42,5 +42,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 /* function prototype */
 int binary_tree_is_leaf(const binary_tree_t *node);
+/* function prototype */
+int binary_tree_is_root(const binary_tree_t *node);
 
 #endif
