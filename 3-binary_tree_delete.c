87f2c54977a64f78e10c65de687be607b4291364
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-04 11:09:29 +0100

    task 3 another attempt

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,11 +2,11 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree != NULL)
-	{
-		if (tree == NULL)
-		    return;
+	if (tree == NULL)
+		return;
 
+	else
+	{
 		binary_tree_delete(tree->left);
 		binary_tree_delete(tree->right);
 		free(tree);
