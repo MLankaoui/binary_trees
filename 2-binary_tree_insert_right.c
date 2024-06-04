de15e5bbfcc4670f4a7674b7e0d8045ac1b7f943
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-04 10:30:25 +0100

    task 2 betty

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,5 +1,14 @@
 #include "binary_trees.h"
 
+/**
+* binary_tree_insert_right - inserts a right child
+*
+* @parent: a parameter to specify the parent
+* @value: to specify the value
+*
+* Descrtiption: a function that inserts a right child
+* Return: the newnode if it doesn t fail. NULL otherwise
+*/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
