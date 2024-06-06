0398a8172d1ba832b6daa627a3d5723087f462af
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-06 12:23:07 +0100

    task 15 first attempt

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -6,10 +6,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 
 	binary_tree_is_full(tree->left);
+	binary_tree_is_full(tree->right);
 
-	if (tree->left != NULL)
+	if (tree->left != NULL || tree->right != NULL)
 		return (1);
 
-    else
-        return (0);
+	else
+		return (0);
 }
